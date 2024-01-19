d2b217e45c1c1d4cb939f55b2287025a02e17f6f
Manuel Peves Livia <mjpeves2002@gmail.com>
2024-01-19 10:21:22 -0500

    feat: submodulo personal

---
+++ src/views/administrador/personal/lista/TablaEncabezado.tsx
@@ -0,0 +1,39 @@
+// ** Next Import
+import Link from 'next/link'
+
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+
+// ** Custom Component Import
+import CustomTextField from 'src/@core/components/mui/text-field'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+import AgregarAlumno from './DrawerAccion'
+import DrawerAccion from './DrawerAccion'
+
+const TablaEncabezado = () => {
+  return (
+    <Box
+      sx={{
+        py: 4,
+        rowGap: 2,
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+      }}
+    >
+      <Button color='secondary' variant='tonal' startIcon={<Icon icon='tabler:upload' />}>
+        Exportar
+      </Button>
+      <Box sx={{ rowGap: 2, display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}>
+        <CustomTextField sx={{ mr: 4 }} placeholder={`Buscar Personal`} />
+        <DrawerAccion accion={'Añadir'} />
+      </Box>
+    </Box>
+  )
+}
+
+export default TablaEncabezado
