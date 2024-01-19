05156c7b24716f85a699f7d46c96207c70269114
Manuel Peves Livia <mjpeves2002@gmail.com>
2024-01-19 10:29:32 -0500

    feat: submodulo reglamento

--- src/navigation/horizontal/index.ts
+++ src/navigation/horizontal/index.ts
@@ -3,21 +3,65 @@ import { HorizontalNavItemsType } from 'src/@core/layouts/types'
 
 const navigation = (): HorizontalNavItemsType => [
   {
-    title: 'Home',
-    path: '/home',
-    icon: 'tabler:smart-home',
+    title: 'Administrador',
+    icon: 'heroicons:user-16-solid',
+    children: [
+      {
+        title: 'Alumno',
+        path: '/administrador/alumno'
+      },
+      {
+        title: 'Tramite',
+        path: '/administrador/tramite'
+      },
+      {
+        title: 'Oficina',
+        path: '/administrador/oficina'
+      },
+      {
+        title: 'Personal',
+        path: '/administrador/personal'
+      },
+      {
+        title: 'Reglamento',
+        path: '/administrador/reglamento'
+      }
+    ]
   },
   {
-    title: 'Second Page',
-    path: '/second-page',
-    icon: 'tabler:mail',
+    title: 'Cargar Nominas OCE',
+    path: '',
+    icon: 'mdi:file-send'
   },
   {
-    path: '/acl',
-    action: 'read',
-    subject: 'acl-page',
-    title: 'Access Control',
-    icon: 'tabler:shield',
+    title: 'Horario',
+    path: '',
+    icon: 'uis:schedule'
+  },
+  {
+    title: 'Bloqueo',
+    icon: 'uis:padlock',
+    children: [
+      {
+        title: 'Bloqueo',
+        path: ''
+      }
+    ]
+  },
+  {
+    title: 'Cargar Preinscripciones',
+    path: '',
+    icon: 'ic:sharp-app-registration'
+  },
+  {
+    title: 'Algoritmo',
+    icon: 'mingcute:settings-2-line',
+    children: [
+      {
+        title: 'Algoritmo',
+        path: ''
+      }
+    ]
   }
 ]
 
--- src/navigation/vertical/index.ts
+++ src/navigation/vertical/index.ts
@@ -25,7 +25,7 @@ const navigation = (): VerticalNavItemsType => {
         },
         {
           title: 'Reglamento',
-          path: '/administrador/invoice/reglamento'
+          path: '/administrador/reglamento'
         }
       ]
     },
